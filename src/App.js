91e231e136f55b2aad367839de430c788e1ec294
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-19 17:28:43 -0500

    add Loading status spinner
    use spinner to indicate that the loading is going on....

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // within the Async function, it is synchronous
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -20,6 +22,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -28,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
