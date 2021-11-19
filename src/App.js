a35bc6b5483dd9a0889448e70ccabcffa84b5a54
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-19 17:34:34 -0500

    006 Handling Loading & Data States
    Display different states to tell user what is going on..

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length == 0 && <p>Found no movies...</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
