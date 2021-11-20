da408d0aa40766df47db6b32ca629a986489da52
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-20 15:04:21 -0500

    009 Preparing The Project For The Next Steps
    Be aware, for Firebase, if the request Movie.json doesn't exist, it will created automatically

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -8,15 +9,18 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // within the Async function, it is synchronous
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      //const response = await fetch("https://swapi.dev/api/films/");   // This API doesn't allow Post
+      const response = await fetch(
+        "https://react-http-69ae2-default-rtdb.firebaseio.com/films.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
+
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -38,6 +42,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
@@ -51,8 +59,12 @@ function App() {
   if (isLoading) {
     content = <p>Loading...</p>;
   }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
